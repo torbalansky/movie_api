303523904a089b313b38bfa4b4926e7703cc9533
torbalansky <torbalansky@gmail.com>
2023-04-23 14:23:03 +0100

    update

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ let allowedOrigins = [
   app.use(cors({
     origin: (origin, callback) => {
       if(!origin) return callback(null, true);
-      if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn't found on the list of allowed origins
         let message = 'The CORS policy for this application doesn/t allow access from origin ' + origin;
         return callback(new Error(message ), false);
       }
