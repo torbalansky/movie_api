1d6a1a8ec3ebf07876e6aaeea32af3940b646ed1
torbalansky <torbalansky@gmail.com>
2023-04-23 14:33:44 +0100

    update

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ app.use(bodyParser.json()); //Sets up body-parser middleware which is used to pa
 app.use(bodyParser.urlencoded({ extended: true })); //Sets up body-parser middleware which is used to parse incoming request in URL
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, Content-Type, X-Auth-Token");
   next();
 });
-
 
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
