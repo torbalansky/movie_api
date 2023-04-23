ecb88f891829bec01c4d8803b5b3c58741b775cc
torbalansky <torbalansky@gmail.com>
2023-04-23 14:46:21 +0100

    update CORS

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const allowedOrigins = ['http://localhost:1234', 'http://localhost:8080', 'https
 
 const corsOptions = {
   origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
