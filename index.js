9c98a91d9332f861693a91f032d3237762936816
torbalansky <torbalansky@gmail.com>
2023-04-23 15:11:04 +0100

    update cors for 1000 time

--- index.js
+++ index.js
@@ -23,7 +23,25 @@ mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnified
 //mongoose.connect('mongodb://127.0.0.1:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const cors = require('cors');
-app.use(cors());
+let allowedOrigins = [
+  'http://localhost:8080',
+  'http://localhost:1234',
+  'https://myflix-torbalansky.netlify.app',
+  'https://torbalansk-myflix-app.herokuapp.com/'
+];
+
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn't found on the list of allowed origins
+      let message = 'The CORS policy for this application does not allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 
 let auth = require('./auth')(app);
 
